290d51ec10a6436dcca95334e03a0a15790fefa2
Firtina Ozbalikci <firtina14@gmail.com>
2013-11-09 22:10:48 +0000

    Initial Commit

---
+++ Editor/AssetUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class AssetUtils {
+    private static readonly string DirectorySeparator = Path.DirectorySeparatorChar.ToString(CultureInfo.InvariantCulture);
+    private readonly string folderName;
+    private readonly string localFolderName;
+    private readonly string fullAssetDirectory;
+
+    public AssetUtils(params string[] baseDirectory)
+    {
+        folderName = CreatePath(baseDirectory);
+        fullAssetDirectory = CreatePath(Application.dataPath, folderName);
+        localFolderName = CreatePath("Assets", folderName);
+    }
+
+
+    private static string CreatePath(params string[] elements)
+    {
+        return String.Join(DirectorySeparator, elements);
+    }
+
+    public static T GetProjectAsset<T>(string assetPath) where T : Object
+    {
+        Object asset = AssetDatabase.LoadAssetAtPath(assetPath, typeof(T));
+
+        if (asset == null)
+            return null;
+
+        return (T)asset;
+    }
+
+    public T GetAsset<T>(string fileName) where T : Object
+    {
+        string assetPath = CreatePath(localFolderName, fileName);
+        return GetProjectAsset<T>(assetPath);
+    }
+
+    public T GetOrCreateAsset<T>(string fileName) where T : ScriptableObject {
+        T asset = GetAsset<T>(fileName) ?? CreateAsset(ScriptableObject.CreateInstance<T>(), fileName);
+        return asset;
+    }
+
+    public T CreateAsset<T>(T asset, string fileName) where T : Object
+    {
+        string fullAssetPath = CreatePath(fullAssetDirectory, fileName);
+        string directoryName = Path.GetDirectoryName(fullAssetPath);
+        string assetPath = CreatePath(localFolderName, fileName);
+        if (directoryName != null)
+        {
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+            AssetDatabase.CreateAsset(asset, assetPath);
+        }
+        else
+        {
+            Debug.LogError("Asset creation failed because of an invalid directory name: " + fullAssetPath);
+        }
+        return asset;
+    }
+}
